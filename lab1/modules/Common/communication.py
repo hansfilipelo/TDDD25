fad16e9834d53722c6078dbb885501e28a1ed015
hansfilipelo <hansfilipelo@gmail.com>
2015-01-29 16:41:25 +0100

    Created proxy object for low level communication.

--- lab1/modules/Common/communication.py
+++ lab1/modules/Common/communication.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import socket
-import time
 import json
 
 class Communication(object):
@@ -17,22 +16,31 @@ class Communication(object):
         try:
             self.serverSocket = socket.create_connection(self.serverInfo)
             self.serverStream = self.serverSocket.makefile(mode="rw")
+            return True
         except socket.error:
-            print("Connection failed... Damn you world!")
+            return False
+    
+    def disconnectFromServer(self):
+        self.serverStream.close()
+        self.serverSocket.close()
+    
+    def send(self,message):
+        try:
+            self.serverStream.write(message + '\n')
+            self.serverStream.flush()
+            return True
+        except:
+            return False
+    
+    def read(self):
+        try:
+            return self.serverStream.readline()
+        except:
+            return False
     
     def test(self):
         sendData = 'balle'
-        self.serverStream.write('sendData' + '\n')
+        self.serverStream.write(sendData + '\n')
         self.serverStream.flush()
-
-
-testObj = Communication(("localhost",46089))
-testObj.connectToServer()
-testObj.test()
-
-out = testObj.serverStream.readline()
-testObj.serverSocket.close()
-print("Answer: " + out)
-
-testObj.serverSocket.close()
+    
 
